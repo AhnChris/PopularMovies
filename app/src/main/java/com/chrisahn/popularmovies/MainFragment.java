2830d4cf8658d8e268ce13161d7bee67706989ac
Christopher Ahn <ahn.chris1@gmail.com>
2017-04-18 23:24:08 -0700

    Initial implementation to use a Spinner as menu item

--- app/src/main/java/com/chrisahn/popularmovies/MainFragment.java
+++ app/src/main/java/com/chrisahn/popularmovies/MainFragment.java
@@ -3,13 +3,19 @@ package com.chrisahn.popularmovies;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
-public class MainFragment extends Fragment implements FetchMovieTask.AsyncResult {
+public class MainFragment extends Fragment implements FetchMovieTask.AsyncResultCallBack {
 
     public MainFragment() {
         // Required empty public constructor
@@ -17,12 +23,33 @@ public class MainFragment extends Fragment implements FetchMovieTask.AsyncResult
 
     @Override
     public void processData(ArrayList<MovieData> arrMovieData) {
-        // Received movie data from onPostExecute
+        // Implement AsyncResultCallBack interface from FetchMovieTask
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu menu layout
+        inflater.inflate(R.menu.fragment_main_menu, menu);
+        // Grab the spinner menu item and set spinner
+        MenuItem menuItem = menu.findItem(R.id.main_fragment_spinner_item);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(menuItem);
+
+        // TODO: Possibly make a custom spinner item & drop-down item layout
+        // Set spinner string array and use default spinner layout
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(getActivity()
+                                                                    ,R.array.sort_options
+                                                                    ,android.R.layout.simple_spinner_item);
+        // Use default spinner drop-down menu layout
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(spinnerAdapter);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
